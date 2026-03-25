5207f3694ed6d4015ee5d9cc273767062c38ecd5
shazyxcl <161633828+shazyxcl@users.noreply.github.com>
2026-03-25 23:40:37 +0700

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,19 @@
 pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-    }
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
-
-rootProject.name = "My Application"
+rootProject.name = "NxDrama"
 
 include(":app")
