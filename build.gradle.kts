ff42b144254ee1f7e46c2c4da3e3efbe87c7138f
shazyxcl <161633828+shazyxcl@users.noreply.github.com>
2026-03-25 22:37:19 +0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("com.android.library") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false  // Upgrade to 2.0.21
-    id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,24 @@
 pluginManagement {
-  repositories {
-    gradlePluginPortal()
-    google()
-    mavenCentral()
-  }
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
-dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-  }
-}
 
 rootProject.name = "My Application"
 
