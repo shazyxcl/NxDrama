ae07cbfa4689f8a8291d679ad1c2aef75954bdeb
shazyxcl <161633828+shazyxcl@users.noreply.github.com>
2026-03-25 19:54:26 +0700

    Create JsonExt.kt

---
+++ app/src/main/java/com/example/myapplication/data/JsonExt.kt
@@ -0,0 +1,21 @@
+package id.sansekai.melolo.data
+
+import com.google.gson.JsonObject
+
+fun JsonObject.getString(key: String): String? {
+    val value = get(key) ?: return null
+    if (value.isJsonNull) return null
+    return runCatching { value.asString }.getOrNull()
+}
+
+fun JsonObject.getInt(key: String): Int? {
+    val value = get(key) ?: return null
+    if (value.isJsonNull) return null
+    return runCatching { value.asInt }.getOrNull()
+}
+
+fun JsonObject.getLong(key: String): Long? {
+    val value = get(key) ?: return null
+    if (value.isJsonNull) return null
+    return runCatching { value.asLong }.getOrNull()
+}
