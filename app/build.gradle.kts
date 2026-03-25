38271dea8104e0edcdb9509da7f0b30bac16802e
shazyxcl <161633828+shazyxcl@users.noreply.github.com>
2026-03-25 22:54:09 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,10 +53,14 @@ dependencies {
     // Compose BOM
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.foundation)
-
+    
+    // Activity Compose - PENTING untuk ComponentActivity dan setContent
+    implementation(libs.androidx.activity.compose)
+    
     // Lifecycle
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -81,4 +85,5 @@ dependencies {
 
     // Debug
     debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
